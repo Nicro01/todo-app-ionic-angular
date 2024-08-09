597a0f5c0a2c58f5e4806a5e21ebe6dc53b00b48
Nicro01 <nicro@edu.unifil.br>
2024-08-08 18:18:11 -0700

    New -> IOS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDjEO89xI4x-b5KaWAviTszKM_o5PnUYA0',
+    authDomain: 'database---react.firebaseapp.com',
+    projectId: 'database---react',
+    storageBucket: 'database---react.appspot.com',
+    messagingSenderId: '296902347623',
+    appId: '1:296902347623:web:f1ba7c33de0c8416d57dc2',
+    measurementId: 'G-YEVR0WVQNL',
+  },
 };
